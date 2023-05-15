3bc5f58badd1b6722a71b3a41cbb415bacfffaf6
jcksnfales <jcksnfales@gmail.com>
2023-05-15 19:56:38 -0400

    Added/updated validation error messages

--- forms.py
+++ forms.py
@@ -3,12 +3,12 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class UserLoginForm(FlaskForm):
-    email = StringField('Email', validators = [DataRequired(), Email()])
+    email = StringField('Email', validators = [DataRequired(), Email(message='Please enter a valid email address.')])
     password = PasswordField('Password', validators = [DataRequired()])
     submit_button = SubmitField('Submit')
 
 class UserRegisterForm(FlaskForm):
-    email = StringField('Email', validators = [DataRequired(), Email()])
-    password = PasswordField('Password', validators = [DataRequired(), EqualTo('password_confirm', message='Passwords must match!')])
+    email = StringField('Email', validators = [DataRequired(), Email(message='Please enter a valid email address.')])
+    password = PasswordField('Password', validators = [DataRequired(), EqualTo('password_confirm', message='Passwords must match.')])
     password_confirm = PasswordField('Confirm Password')
     submit_button = SubmitField('Submit')
