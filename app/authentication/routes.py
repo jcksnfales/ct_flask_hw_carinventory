91d0845ec52881630a99d3cbbbf751a00c02cbb9
jcksnfales <jcksnfales@gmail.com>
2023-05-15 16:18:50 -0400

    Separated login/registration forms
    - Created distinct form objects
    - Removed forms.html and added content to sign_in.html and sign_up.html
    + Added password confirmation field to signup form
    + Added error message display for signup form

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,4 +1,4 @@
-from forms import UserLoginForm
+from forms import UserLoginForm, UserRegisterForm
 from models import User, db, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
@@ -7,7 +7,7 @@ auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    form = UserLoginForm()
+    form = UserRegisterForm()
 
     try:
         if request.method == 'POST' and form.validate_on_submit():
@@ -16,12 +16,13 @@ def signup():
             print(email, password)
 
             user = User(email, password = password)
-
             db.session.add(user)
             db.session.commit()
 
             flash(f'You have successfully created a user account {email}', 'User-created')
             return redirect(url_for('auth.signin'))
+        # elif request.method == 'POST' and not form.validate_on_submit():
+        #     print(form.password.errors[0])
     except:
         raise Exception('Invalid form data: Please check your form')
     return render_template('sign_up.html', form=form)
