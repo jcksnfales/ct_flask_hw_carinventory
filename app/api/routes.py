68e80680d27ae3cbeb82d7a9951b449fcbeeabbf
jcksnfales <jcksnfales@gmail.com>
2023-05-16 17:30:43 -0400

    Added /api/cars POST route

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,23 @@
 from flask import Blueprint, request, jsonify, render_template
 from helpers import token_required
-from models import db, User #, Contact, contact_schema, contacts_schema
+from models import db, User, Car, car_schema, cars_schema
 
 api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/cars', methods = ['POST'])
+@token_required
+def add_car(current_user_token):
+    nickname = request.json['nickname']
+    make = request.json['make']
+    model = request.json['model']
+    prodyear = request.json['prodyear']
+    mileage = request.json['mileage']
+    user_token = current_user_token.token
+
+    new_car = Car(nickname, make, model, prodyear, mileage, user_token=user_token)
+
+    db.session.add(new_car)
+    db.session.commit()
+
+    response = car_schema.dump(new_car)
+    return jsonify(response)
