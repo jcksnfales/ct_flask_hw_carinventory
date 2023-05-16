bfc7f3600c62612901b51dc8f37c3b13cc7bdf0f
jcksnfales <jcksnfales@gmail.com>
2023-05-16 16:35:29 -0400

    Added comments, rephrased error/exception messages

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -14,11 +14,15 @@ def signup():
             email = form.email.data
             password = form.password.data
 
+            # find existing users' emails with a database query
             existing_users = [e[0] for e in db.session.query(User.email).all()]
+            # then, check entered email against existing emails
             if email in existing_users:
+                # if there's already an account registered with that email, return the user an error
                 flash(f'"{email}" has already been registered.', category='auth-register-failed')
                 return redirect(url_for('auth.signup'))
             else:
+                # otherwise, register the email
                 user = User(email, password = password)
                 db.session.add(user)
                 db.session.commit()
@@ -26,7 +30,7 @@ def signup():
                 flash(f'Successfully registered "{email}"', category='auth-register-success')
                 return redirect(url_for('auth.signin'))
     except:
-        raise Exception('Invalid form data: Please check your form')
+        raise Exception('Invalid Form Data')
     return render_template('sign_up.html', form=form)
 
 @auth.route('/signin', methods = ['GET', 'POST'])
@@ -38,16 +42,20 @@ def signin():
             email = form.email.data
             password = form.password.data
 
+            # query the database for the given user's record
             logged_user = User.query.filter(User.email == email).first()
+            # check if there is an existing record for the given user, and check the given password against the recorded one
             if logged_user and check_password_hash(logged_user.password, password):
+                # if the username and password are valid, log user in and return a success message
                 login_user(logged_user)
                 flash('Successfully logged in.', 'auth-login-success')
                 return redirect(url_for('site.profile'))
             else:
-                flash('Login failed.', category='auth-login-failed')
+                # if the username/password are NOT valid, return the user an error message
+                flash('Incorrect username or password.', category='auth-login-failed')
                 return redirect(url_for('auth.signin'))
     except:
-        raise Exception('Invalid Form Data: Please Check your Form')
+        raise Exception('Invalid Form Data')
     return render_template('sign_in.html', form=form)
 
 @auth.route('/logout')
