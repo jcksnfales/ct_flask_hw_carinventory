ef352b84a096abc35a91f34789ddaddb699a8150
jcksnfales <jcksnfales@gmail.com>
2023-05-19 17:14:37 -0400

    Added images to landing page

--- app/site/routes.py
+++ app/site/routes.py
@@ -14,7 +14,7 @@ def profile():
     if current_user.is_authenticated:
         # if user is logged in, query the database for cars owned by them
         queried_cars = Car.query.filter_by(user_token=current_user.token).all()
-        # direct the user to /profile, passing in their cars as 'user_cars' and their schema dumps as 'user_cars_json'
+        # direct the user to /profile, passing in their cars as 'user_cars' and the cars' schema dumps as 'user_cars_json'
         return render_template('profile.html', user_cars=queried_cars, user_cars_json=cars_schema.dump(queried_cars))
     else:
         # otherwise, redirect them to the landing page
